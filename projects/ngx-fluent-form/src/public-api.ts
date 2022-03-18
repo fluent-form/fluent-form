e7876226c6d9c0973d7d430ce100aa8284d10bd4
HyperLife1119 <hyperlife1119@qq.com>
2022-03-18 21:58:01 +0800

    feat: export all directives

--- projects/ngx-fluent-form/src/public-api.ts
+++ projects/ngx-fluent-form/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of ngx-fluent-form
  */
 
+export * from './lib/directives/fluent-event-binder.directive';
+export * from './lib/directives/fluent-property-binder.directive';
 export * from './lib/fluent-form.component';
 export * from './lib/fluent-form.control';
 export * from './lib/fluent-form.interface';

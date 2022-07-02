386991bc41841a7394b07b02533a7cb838a62eb5
HyperLife1119 <hyperlife1119@qq.com>
2022-07-02 15:00:51 +0800

    chore: 调整目录

--- projects/ngx-fluent-form/src/public-api.ts
+++ projects/ngx-fluent-form/src/public-api.ts
@@ -2,9 +2,10 @@
  * Public API Surface of ngx-fluent-form
  */
 
-export * from './lib/directives/fluent-event-binder.directive';
-export * from './lib/directives/fluent-property-binder.directive';
-export * from './lib/fluent-form.builder';
-export * from './lib/fluent-form.component';
-export * from './lib/fluent-form.module';
-export * from './lib/schemas';
+export * from './directives/fluent-event-binder.directive';
+export * from './directives/fluent-property-binder.directive';
+export * from './fluent-form.builder';
+export * from './fluent-form.component';
+export * from './fluent-form.module';
+export * from './schemas';
+

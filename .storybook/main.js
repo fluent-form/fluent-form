4dd781ee4e89bfb3371921f3cf3542cf36fae007
HyperLife1119 <hyperlife1119@qq.com>
2022-07-05 18:40:57 +0800

    feat(storybook): update more stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,27 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    // {
+    //   name: '@storybook/addon-storysource',
+    //   options: {
+    //     rule: {
+    //       include: [path.resolve(__dirname, '../projects/ngx-fluent-form/src')], // You can specify directories
+    //     },
+    //     loaderOptions: {
+    //       parser: 'typescript',
+    //       prettierConfig: { printWidth: 80, singleQuote: false },
+    //     },
+    //   },
+    // },
   ],
-  "framework": "@storybook/angular",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/angular',
+  core: {
+    builder: '@storybook/builder-webpack5'
   }
 }
--- stories/fluent-form-advanced.stories.ts
+++ stories/fluent-form-advanced.stories.ts
@@ -1,7 +1,9 @@
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { Component } from '@angular/core';
-import { form, input, select } from 'ngx-fluent-form';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { form, input, radio, select } from 'ngx-fluent-form';
+import { findSchema } from 'projects/ngx-fluent-form/src/utils/schema.utils';
 import { AbstractFluentFormWrapperComponent } from './components/abstract-fluent-form-wrapper.component';
 import { SELECT_OPTIONS } from './control.options';
 import { createMeta, createStory } from './storybook.utils';
@@ -21,20 +23,34 @@ registerLocaleData(zh);
       [spinSize]="spinSize"></fluent-form>
     <pre>{{ model | json }}</pre>
   `,
-  styleUrls: ['./components/abstract-fluent-form-wrapper.component.scss']
+  styleUrls: ['./components/abstract-fluent-form-wrapper.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 class FluentFormWrapperComponent extends AbstractFluentFormWrapperComponent {
-  override schemas = form(
-    select('select').label('选择器').options(SELECT_OPTIONS).span(8).listener({
-      valueChange: value => {
-        this.model.text = value;
-        this.model = { ...this.model };
-      }
-    }),
-    input('text').label('文本输入框').span(8),
-  );
+  constructor() {
+    super();
 
-  override model: { text?: string, select?: string } = {}
+    this.schemas = form(
+      select('select').label('控制内容').options(SELECT_OPTIONS).span(8).listener({
+        valueChange: value => {
+          this.model.text = value;
+          this.model = { ...this.model };
+        }
+      }),
+      radio('radio').label('控制显隐').span(8).options([
+        { label: '显示', value: false },
+        { label: '隐藏', value: true },
+      ]).value(false).listener({
+        valueChange: value => {
+          findSchema(this.schemas, 'text')!.hidden = value;
+          this.schemas = this.schemas.slice()
+        }
+      }),
+      input('text').label('文本输入框').span(8),
+    );
+
+    this.model = {}
+  }
 }
 
 export default createMeta({
@@ -43,3 +59,46 @@ export default createMeta({
 });
 
 export const LinkageForm = createStory<FluentFormWrapperComponent>({});
+
+export const FormValidation = createStory<FluentFormWrapperComponent>({
+  args: {
+    schemas: form(
+      input('username')
+        .label('Username')
+        .required(true)
+        .span(12)
+        .feedback(true)
+        .tips({
+          auto: {
+            default: {
+              required: 'Please enter your username!'
+            }
+          }
+        }),
+      input('email')
+        .label('Email')
+        .subtype('email')
+        .required(true)
+        .validator([Validators.email])
+        .span(12)
+        .feedback(true)
+        .tips({
+          auto: {
+            default: {
+              required: 'Please enter your Email!',
+              email: 'The enter is not valid E-mail!',
+            }
+          }
+        }),
+      input('password')
+        .label('Password')
+        .subtype('password')
+        .required(true)
+        .span(12)
+        .feedback(true)
+        .tips({
+          error: 'Please enter your password!'
+        }),
+    )
+  }
+});

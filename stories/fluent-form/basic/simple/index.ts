f204cedb79c13dc5763fa5125aacc1a2e077ec44
HyperLife1119 <hyperlife1119@qq.com>
2022-07-08 18:28:24 +0800

    feat(storybook): update story

--- stories/fluent-form/basic/simple/index.ts
+++ stories/fluent-form/basic/simple/index.ts
@@ -1,4 +1,4 @@
-import { cascader, checkbox, date, datetime, form, input, number, radio, range, rate, select, slider, switcher, textarea, time } from 'ngx-fluent-form';
+import { cascader, checkbox, checkboxGroup, date, datetime, form, input, number, radio, range, rate, select, slider, switcher, textarea, time } from 'ngx-fluent-form';
 import { CASCADER_OPTIONS, CHECKBOX_OPTIONS, RADIO_OPTIONS, SELECT_OPTIONS } from 'stories/options';
 import { defineStory } from 'stories/storybook';
 import dedent from 'ts-dedent';
@@ -6,7 +6,7 @@ import dedent from 'ts-dedent';
 export const story = defineStory({
   args: {
     schemas: form(
-      input('text').label('文本输入框').span(24),
+      input('text').label('文本输入框').span(24).id('ipt'),
       textarea('textarea').label('文本框').span(24),
       number('number').label('数字输入框').span(24),
       date('date').label('日期录入框').span(24),
@@ -14,8 +14,9 @@ export const story = defineStory({
       datetime('datetime').label('日期时间录入框').span(24),
       time('time').label('时间录入框').span(24),
       switcher('switch').label('开关').placeholder(['启用', '禁用']).value(true).span(24),
-      radio('radioGroup').label('单选框组').options(RADIO_OPTIONS).span(24),
-      checkbox('checkboxGroup').label('复选框组').options(CHECKBOX_OPTIONS).span(24),
+      radio('radio').label('单选框组').options(RADIO_OPTIONS).span(24),
+      checkbox('checkbox').label('单个复选框').content('同意').span(24),
+      checkboxGroup('checkboxGroup').label('复选框组').options(CHECKBOX_OPTIONS).span(24),
       select('select').label('选择器').options(SELECT_OPTIONS).span(24),
       cascader('cascader').label('联级选择器').options(CASCADER_OPTIONS).span(24),
       rate('rate').label('评分').value(2.5).span(24),
@@ -27,7 +28,7 @@ export const story = defineStory({
 
 export const source = dedent`
   import { Component } from '@angular/core';
-  import { cascader, checkbox, date, datetime, form, input, number, radio, range, rate, select, slider, switcher, textarea, time } from 'ngx-fluent-form';
+  import { cascader, checkbox, checkboxGroup, date, datetime, form, input, number, radio, range, rate, select, slider, switcher, textarea, time } from 'ngx-fluent-form';
 
   @Component({
     selector: 'example-component',
@@ -43,8 +44,9 @@ export const source = dedent`
       datetime('datetime').label('日期时间录入框').span(24),
       time('time').label('时间录入框').span(24),
       switcher('switch').label('开关').placeholder(['启用', '禁用']).value(true).span(24),
-      radio('radioGroup').label('单选框组').options(RADIO_OPTIONS).span(24),
-      checkbox('checkboxGroup').label('复选框组').options(CHECKBOX_OPTIONS).span(24),
+      radio('radio').label('单选框组').options(RADIO_OPTIONS).span(24),
+      checkbox('checkbox').label('单个复选框').content('同意').span(24),
+      checkboxGroup('checkboxGroup').label('复选框组').options(CHECKBOX_OPTIONS).span(24),
       select('select').label('选择器').options(SELECT_OPTIONS).span(24),
       cascader('cascader').label('联级选择器').options(CASCADER_OPTIONS).span(24),
       rate('rate').label('评分').value(2.5).span(24),

ab749c61dc5af2dd32f7aa2660dd895e6fdff326
HyperLife1119 <hyperlife1119@qq.com>
2022-07-08 14:51:21 +0800

    feat(storybook): update more stories

---
+++ stories/fluent-form/advanced/double-property/index.ts
@@ -0,0 +1,29 @@
+import { form, range } from 'ngx-fluent-form';
+import { defineStory } from 'stories/storybook';
+import dedent from 'ts-dedent';
+
+export const story = defineStory({
+  args: {
+    schemas: form(
+      range(['start', 'end']).label('日期区间控件').span(12),
+    ),
+    model: {}
+  }
+});
+
+export const source = dedent`
+  import { Component } from '@angular/core';
+  import { form, range } from 'ngx-fluent-form';
+
+  @Component({
+    selector: 'example-component',
+    template: \`<fluent-form [schemas]="schemas" [model]="model"></fluent-form>\`
+  })
+  export class ExampleComponent {
+    schemas: form(
+      range(['start', 'end']).label('日期区间控件').span(12),
+    ),
+
+    model = {};
+  }
+`;
---
+++ stories/fluent-form/basic/json/index.ts
@@ -0,0 +1,60 @@
+import { defineStory } from 'stories/storybook';
+import dedent from 'ts-dedent';
+
+export const story = defineStory({
+  args: {
+    schemas: [
+      { type: 'input', name: 'username', label: '用户名', placeholder: '请输入用户名', span: 12 },
+      { type: 'input', name: 'password', label: '密码', placeholder: '请输入密码', subtype: 'password', span: 12 },
+    ],
+    model: {}
+  },
+});
+
+export const jsonSource = dedent`
+  import { Component } from '@angular/core';
+  import { AnySchema } from 'ngx-fluent-form';
+
+  @Component({
+    selector: 'example-component',
+    template: \`<fluent-form [schemas]="schemas" [model]="model"></fluent-form>\`
+  })
+  export class ExampleComponent {
+    schemas: AnySchema[] = [
+      { type: 'input', name: 'username', label: '用户名', placeholder: '请输入用户名', span: 12 },
+      { type: 'input', name: 'password', label: '密码', placeholder: '请输入密码', subtype: 'password', span: 12 },
+    ];
+
+    model = {};
+  }
+`;
+
+export const jsonAndFluentSource = dedent`
+  import { Component } from '@angular/core';
+  import { AnySchema, input } from 'ngx-fluent-form';
+
+  @Component({...})
+  export class ExampleComponent {
+    schemas: AnySchema[] = [
+      input('username').label('用户名').placeholder('请输入用户名').span(12).build(), // 👈 call build() method
+      { type: 'input', name: 'password', label: '密码', placeholder: '请输入密码', subtype: 'password', span: 12 },
+    ];
+
+    model = {};
+  }
+`;
+
+export const fluentAndJsonSource = dedent`
+  import { Component } from '@angular/core';
+  import { form, input } from 'ngx-fluent-form';
+
+  @Component({...})
+  export class ExampleComponent {
+    schemas = form(
+      { type: 'input', name: 'username', label: '用户名', placeholder: '请输入用户名', span: 12 },
+      input('password').label('密码').placeholder('请输入密码').subtype('password').span(12),
+    );
+
+    model = {};
+  }
+`;

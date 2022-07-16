cf64446c40f8e9bfd86b5ebf4e10a14a1dc61368
HyperLife1119 <hyperlife1119@qq.com>
2022-07-16 10:36:06 +0800

    feat: add schema utils & control schema utils

--- projects/ngx-fluent-form/src/utils/schema.utils.ts
+++ projects/ngx-fluent-form/src/utils/schema.utils.ts
@@ -1,4 +1,4 @@
-import { ValidatorFn, Validators } from '@angular/forms';
+import { AsyncValidatorFn, ValidatorFn, Validators } from '@angular/forms';
 import { COMPONENT_CONTAINER_SCHEMA_TYPES, COMPONENT_SCHEMA_TYPES, CONTROL_CONTAINER_SCHEMA_TYPES, TEXT_CONTROL_SCHEMA_TYPES } from '../constants';
 import { InputControlSchema, TextareaControlSchema } from '../schemas';
 import { AnySchemaName, SingleKeySchemaName } from '../schemas/abstract.schema';
@@ -46,24 +46,6 @@ export const isDoubleKeySchema = (schema: AnySchema): schema is DoubleKeyControl
 );
 
 /**
- * 添加验证器到图示中
- * @internal
- * @param schema
- * @param validator
- */
-const addValidatorToSchema = <T extends ControlSchema>(schema: T, validator: ValidatorFn | ValidatorFn[]) => {
-  const validators = Array.isArray(validator) ? validator : [validator];
-
-  if (schema.validator) {
-    schema.validator = schema.validator.concat(validators);
-  } else {
-    schema.validator = validators;
-  }
-
-  return schema;
-}
-
-/**
  * 标准化容器图示
  * @internal
  * @param schema
@@ -87,24 +69,26 @@ const standardContainerSchema = <T extends AnyContainerSchema>(schema: T): T => 
  * @param schema
  */
 const standardTextControlSchema = <T extends InputControlSchema | TextareaControlSchema>(schema: T): T => {
+  const utils = controlSchemaUtils(schema);
+
   if (schema.type === 'input' && schema.subtype === 'email') {
-    schema = addValidatorToSchema(schema, Validators.email);
+    utils.addValidator(Validators.email);
   }
 
   if (schema.length) {
     if (typeof schema.length === 'number') {
-      schema = addValidatorToSchema(schema, [
+      utils.addValidator([
         Validators.minLength(schema.length),
         Validators.maxLength(schema.length)
       ]);
     } else {
       const { min, max } = schema.length as { max?: number, min?: number };
       if (min) {
-        schema = addValidatorToSchema(schema, Validators.minLength(min));
+        utils.addValidator(Validators.minLength(min));
       }
 
       if (max) {
-        schema = addValidatorToSchema(schema, Validators.maxLength(max));
+        utils.addValidator(Validators.maxLength(max));
       }
     }
   }
@@ -126,7 +110,7 @@ export const standardSchema = <T extends AnySchema>(schema: T | Builder<T, T, {}
   }
 
   if ('required' in _schema && _schema.required) {
-    _schema = addValidatorToSchema(_schema as ControlSchema, Validators.required);
+    controlSchemaUtils(_schema as ControlSchema).addValidator(Validators.required);
   }
 
   return _schema as T;
@@ -140,6 +124,70 @@ export const standardSchemas = (schemas: (AnySchema | Builder<AnySchema, AnySche
   schemas.map(schema => standardSchema(schema))
 );
 
+export function controlSchemaUtils<S extends ControlSchema>(schema: S) {
+  return new ControlSchemaUtils(schema);
+}
+
+export class ControlSchemaUtils<S extends ControlSchema> {
+  constructor(private readonly schema: S) { }
+
+  addValidator(validator: ValidatorFn | ValidatorFn[]) {
+    const validators = Array.isArray(validator) ? validator : [validator];
+
+    if (this.schema.validator) {
+      this.schema.validator = this.schema.validator.concat(validators);
+    } else {
+      this.schema.validator = validators;
+    }
+
+    return this;
+  }
+
+  addAsyncValidator(validator: AsyncValidatorFn | AsyncValidatorFn[]) {
+    const validators = Array.isArray(validator) ? validator : [validator];
+
+    if (this.schema.asyncValidator) {
+      this.schema.asyncValidator = this.schema.asyncValidator.concat(validators);
+    } else {
+      this.schema.asyncValidator = validators;
+    }
+
+    return this;
+  }
+}
+
+export function schemasUtils<S extends AnySchema[]>(schemas: S) {
+  return new SchemasUtils(schemas);
+}
+
+export class SchemasUtils<S extends AnySchema[]> {
+  constructor(private readonly schemas: S) { }
+
+  find<T extends AnySchema>(name: AnySchemaName): T | undefined;
+  find<T extends AnySchema>(path: [...SingleKeySchemaName[], AnySchemaName]): T | undefined;
+  find<T extends AnySchema>(path: AnySchemaName | [...SingleKeySchemaName[], AnySchemaName]): T | undefined;
+  find<T extends AnySchema>(path: AnySchemaName | [...SingleKeySchemaName[], AnySchemaName]): T | undefined {
+    let schemas = this.schemas as AnySchema[];
+    // 如果是数组，那么除了最后一个元素，其他元素所对应的 schema 一定是 container schema
+    if (Array.isArray(path)) {
+      const [endPath, ...beforePath] = path.reverse() as [AnySchemaName, ...SingleKeySchemaName[]];
+      schemas = beforePath.reduceRight((schemas, name) => (
+        (schemas.find(o => o.name === name) as AnyContainerSchema).schemas as AnyControlSchema[]
+      ), schemas as AnyControlSchema[]);
+      path = endPath;
+    }
+
+    return schemas.find(o => {
+      // 处理双字段模式
+      if (Array.isArray(o.name) && Array.isArray(path)) {
+        return arraysEqual(o.name, path);
+      }
+
+      return o.name === path;
+    }) as T | undefined;
+  }
+}
+
 /** @internal */
 function arraysEqual(a: unknown[], b: unknown[]): boolean {
   if (a === b) return true;
@@ -151,32 +199,4 @@ function arraysEqual(a: unknown[], b: unknown[]): boolean {
   }
 
   return true;
-}
-
-/**
- * 从图示列表中搜寻子图示
- * @param schemas
- * @param name
- */
-export function findSchema<T extends AnySchema = AnySchema>(schemas: AnySchema[], name: AnySchemaName): T | undefined;
-export function findSchema<T extends AnySchema = AnySchema>(schemas: AnySchema[], path: [...SingleKeySchemaName[], AnySchemaName]): T | undefined;
-export function findSchema<T extends AnySchema = AnySchema>(schemas: AnySchema[], path: AnySchemaName | [...SingleKeySchemaName[], AnySchemaName]): T | undefined;
-export function findSchema<T extends AnySchema = AnySchema>(schemas: AnySchema[], path: AnySchemaName | [...SingleKeySchemaName[], AnySchemaName]): T | undefined {
-  // 如果是数组，那么除了最后一个元素，其他元素所对应的 schema 一定是 container schema
-  if (Array.isArray(path)) {
-    const [endPath, ...beforePath] = path.reverse() as [AnySchemaName, ...SingleKeySchemaName[]];
-    schemas = beforePath.reduceRight((schemas, name) => (
-      (schemas.find(o => o.name === name) as AnyContainerSchema).schemas as AnyControlSchema[]
-    ), schemas as AnyControlSchema[]);
-    path = endPath;
-  }
-
-  return schemas.find(o => {
-    // 处理双字段模式
-    if (Array.isArray(o.name) && Array.isArray(path)) {
-      return arraysEqual(o.name, path);
-    }
-
-    return o.name === path;
-  }) as T | undefined;
 }

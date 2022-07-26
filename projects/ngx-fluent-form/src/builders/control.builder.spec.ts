48694404051685990a33092ccdd2daddb994cfcf
HyperLife1119 <hyperlife1119@qq.com>
2022-07-26 10:14:24 +0800

    test: optimize test

--- projects/ngx-fluent-form/src/builders/control.builder.spec.ts
+++ projects/ngx-fluent-form/src/builders/control.builder.spec.ts
@@ -1,112 +1,111 @@
 import { SafeAny } from '@ngify/types';
 import { CheckboxControlSchema, DatePickerControlSchema, InputControlSchema, NumberInputControlSchema, RangePickerControlSchema, RateControlSchema, SliderControlSchema, SwitchControlSchema, TextareaControlSchema, TimePickerControlSchema } from '../schemas';
-import { standardSchema } from '../utils';
-import { array, cascader, checkbox, checkboxGroup, date, datetime, group, input, number, radio, range, rate, select, slider, switcher, textarea, time } from './control.builder';
+import { array, cascader, checkbox, checkboxGroup, control, date, datetime, group, input, number, radio, range, rate, select, slider, switcher, textarea, time } from './control.builder';
 
 describe('control.builder', () => {
   it('input', () => {
-    const schema = standardSchema(input('input'));
+    const schema = control(input('input'));
     const value = { type: 'input', name: 'input' } as InputControlSchema<'input', string>;
     expect(schema).toEqual(value);
   });
 
   it('textarea', () => {
-    const schema = standardSchema(textarea('textarea'));
-    const value = standardSchema({ type: 'textarea', name: 'textarea' }) as TextareaControlSchema<'textarea', string>;
+    const schema = control(textarea('textarea'));
+    const value = control({ type: 'textarea', name: 'textarea' }) as TextareaControlSchema<'textarea', string>;
     expect(schema).toEqual(value);
   });
 
   it('number', () => {
-    const schema = standardSchema(number('number'));
+    const schema = control(number('number'));
     const value = { type: 'number', name: 'number' } as NumberInputControlSchema<'number', number>;
     expect(schema).toEqual(value);
   });
 
   it('date', () => {
-    const schema = standardSchema(date('date'));
+    const schema = control(date('date'));
     const value = { type: 'date', name: 'date' } as DatePickerControlSchema<'date', Date>;
     expect(schema).toEqual(value);
   });
 
   it('datetime', () => {
-    const schema = standardSchema(datetime('datetime'));
+    const schema = control(datetime('datetime'));
     const value = {
       type: 'date',
       name: 'datetime',
       format: 'yyyy-MM-dd HH:mm:ss',
-      showTime: true
+      time: true
     } as DatePickerControlSchema<'datetime', Date>;
     expect(schema).toEqual(value);
   });
 
   it('time', () => {
-    const schema = standardSchema(time('time'));
+    const schema = control(time('time'));
     const value = { type: 'time', name: 'time' } as TimePickerControlSchema<'time', Date>;
     expect(schema).toEqual(value);
   });
 
   it('switcher', () => {
-    const schema = standardSchema(switcher('switcher'));
+    const schema = control(switcher('switcher'));
     const value = { type: 'switch', name: 'switcher' } as SwitchControlSchema<'switcher', boolean>;
     expect(schema).toEqual(value);
   });
 
   it('select', () => {
-    const schema = standardSchema(select('select').options([]));
+    const schema = control(select('select').options([]));
     const value = { type: 'select', name: 'select', options: [] } as SafeAny;
     expect(schema).toEqual(value);
   });
 
   it('cascader', () => {
-    const schema = standardSchema(cascader('cascader').options([]));
+    const schema = control(cascader('cascader').options([]));
     const value = { type: 'cascader', name: 'cascader', options: [] } as SafeAny;
     expect(schema).toEqual(value);
   });
 
   it('radio', () => {
-    const schema = standardSchema(radio('radio').options([]));
+    const schema = control(radio('radio').options([]));
     const value = { type: 'radio', name: 'radio', options: [] } as SafeAny;
     expect(schema).toEqual(value);
   });
 
   it('checkbox', () => {
-    const schema = standardSchema(checkbox('checkbox'));
+    const schema = control(checkbox('checkbox'));
     const value = { type: 'checkbox', name: 'checkbox' } as CheckboxControlSchema<'checkbox', boolean>;
     expect(schema).toEqual(value);
   });
 
   it('checkboxGroup', () => {
-    const schema = standardSchema(checkboxGroup('checkboxGroup').options([]));
+    const schema = control(checkboxGroup('checkboxGroup').options([]));
     const value = { type: 'checkbox-group', name: 'checkboxGroup', options: [] } as SafeAny;
     expect(schema).toEqual(value);
   });
 
   it('rate', () => {
-    const schema = standardSchema(rate('rate'));
+    const schema = control(rate('rate'));
     const value = { type: 'rate', name: 'rate' } as RateControlSchema<'rate', number>;
     expect(schema).toEqual(value);
   });
 
   it('slider', () => {
-    const schema = standardSchema(slider('slider'));
+    const schema = control(slider('slider'));
     const value = { type: 'slider', name: 'slider' } as SliderControlSchema<'slider', number | [number, number]>;
     expect(schema).toEqual(value);
   });
 
   it('range', () => {
-    const schema = standardSchema(range('range'));
+    const schema = control(range('range'));
     const value = { type: 'range', name: 'range' } as RangePickerControlSchema<'range', [Date, Date]>;
     expect(schema).toEqual(value);
   });
 
   it('group', () => {
-    const schema = standardSchema(group('group').schemas());
+    const schema = control(group('group').schemas());
     const value = { type: 'group', name: 'group', schemas: [] } as SafeAny;
     expect(schema).toEqual(value);
   });
 
   it('array', () => {
-    const schema = standardSchema(array('array').schemas());
+    const schema = control(array('array').schemas());
     const value = { type: 'array', name: 'array', schemas: [] } as SafeAny;
     expect(schema).toEqual(value);
   });
--- projects/ngx-fluent-form/src/components/fluent-form/fluent-form.component.spec.ts
+++ projects/ngx-fluent-form/src/components/fluent-form/fluent-form.component.spec.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { form, input } from '../../builders';
+import { array, button, form, group, input, inputGroup } from '../../builders';
 import { FluentFormModule } from '../../fluent-form.module';
 import { AnySchema } from '../../schemas/index.schema';
 import { Obj } from '../../type';
@@ -55,5 +55,26 @@ describe('FluentFormComponent', () => {
     fixture.detectChanges();
 
     expect(component.model).toEqual({ text: 'test' });
+  });
+
+  it('应该能正确处理控件的 disabled 选项', () => {
+    component.schemas = form(
+      inputGroup().schemas(
+        input('text').disabled(() => true)
+      ),
+      group('group').schemas(
+        input('text').disabled(() => false)
+      ),
+      array('array').schemas(
+        input().disabled(true)
+      ),
+      button().content('btn')
+    );
+    component.model = { text: 'test' };
+    fixture.detectChanges();
+
+    expect(component.target.form.get('text')?.disabled).toEqual(true);
+    expect(component.target.form.get('group.text')?.disabled).toEqual(false);
+    expect(component.target.form.get(['array', 0])?.disabled).toEqual(true);
   });
 });

0380f2f17698530274bda104b45bea0bac75e3b5
HyperLife1119 <hyperlife1119@qq.com>
2022-07-26 11:55:18 +0800

    test: fix test

--- projects/ngx-fluent-form/src/directives/event-binder.directive.spec.ts
+++ projects/ngx-fluent-form/src/directives/event-binder.directive.spec.ts
@@ -1,9 +1,13 @@
 import { ElementRef } from '@angular/core';
+import { Destroyer } from '../services/destroyer.service';
 import { EventBinderDirective } from './event-binder.directive';
 
 describe('EventBinderDirective', () => {
   it('should create an instance', () => {
-    const directive = new EventBinderDirective(new ElementRef({} as HTMLElement));
+    const directive = new EventBinderDirective(
+      new ElementRef({} as HTMLElement),
+      new Destroyer()
+    );
     expect(directive).toBeTruthy();
   });
 });

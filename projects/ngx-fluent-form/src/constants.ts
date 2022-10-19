4551114a9f682bfe84059437171f0aa928033012
HyperLife1119 <hyperlife1119@qq.com>
2022-10-19 11:24:41 +0800

    fix: fix bug

--- projects/ngx-fluent-form/src/constants.ts
+++ projects/ngx-fluent-form/src/constants.ts
@@ -7,10 +7,10 @@ export const CONTROL_CONTAINER_SCHEMA_TYPES = [
   'step',
   'tabset',
   'tab'
-] as const;
+];
 /** 文本控件图示类型 */
-export const TEXT_CONTROL_SCHEMA_TYPES = ['input', 'textarea'] as const;
+export const TEXT_CONTROL_SCHEMA_TYPES = ['input', 'textarea'];
 /** 组件容器图示类型 */
-export const COMPONENT_CONTAINER_SCHEMA_TYPES = ['button-group'] as const;
+export const COMPONENT_CONTAINER_SCHEMA_TYPES = ['button-group'];
 /** 普通组件图示类型 */
-export const COMPONENT_SCHEMA_TYPES = ['button', 'text'] as const;
+export const COMPONENT_SCHEMA_TYPES = ['button', 'text'];

3af7422965913c937314ca3567436041a71d9542
HyperLife1119 <hyperlife1119@qq.com>
2022-10-27 14:27:32 +0800

    docs: 移除可变模型用法的文档内容

--- stories/fluent-form/advanced/mutable/index.ts
+++
@@ -1,2 +0,0 @@
-export const mutableSource = `<fluent-form [schemas]="schemas" [model]="model"></fluent-form>`;
-export const immutableSource = `<fluent-form [schemas]="schemas" [(model)]="model"></fluent-form>`;

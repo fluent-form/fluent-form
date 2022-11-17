506ef4712822fd1e48ca70fc24c2796c410664bf
HyperLife1119 <hyperlife1119@qq.com>
2022-11-17 16:11:39 +0800

    refactor(utils): improve builder function and type definition

--- projects/ngx-fluent-form/src/utils/builder.utils.ts
+++ projects/ngx-fluent-form/src/utils/builder.utils.ts
@@ -1,16 +1,14 @@
 import { SafeAny } from '@ngify/types';
 import { AnyObject } from '../types';
 
-const REST_PARAMETERS = ['schemas', 'validators', 'asyncValidators'] as const;
-
-export function builder<T>(target: Partial<T> = {}): Builder<T> {
+function _builder<T>(target: Partial<T> = {}, rests: readonly (keyof T)[]): Builder<T> {
   const builder = new Proxy(target as AnyObject, {
     get(target, prop: string) {
       if ('build' === prop) {
         return () => target;
       }
 
-      if (REST_PARAMETERS.includes(prop as typeof REST_PARAMETERS[number])) {
+      if (rests.includes(prop as keyof T)) {
         return (...args: unknown[]): unknown => {
           target[prop] = args;
           return builder;
@@ -29,6 +27,13 @@ export function builder<T>(target: Partial<T> = {}): Builder<T> {
   return builder as Builder<T>;
 }
 
+
+export function builder<T>(target: Partial<T> = {}): Builder<T> {
+  return _builder(target, REST_PARAMETERS as unknown as (keyof T)[]);
+}
+
+const REST_PARAMETERS = ['schemas', 'validators', 'asyncValidators'] as const;
+
 type Buildable<T> = { build: () => T };
 type RestParams = undefined | SafeAny[];
 type DefaultRestParamsName = typeof REST_PARAMETERS[number];
@@ -39,17 +44,24 @@ type DefaultRestParamsName = typeof REST_PARAMETERS[number];
  * @template C candidate 候选
  * @template R rest 剩余参数名
  */
-export type Builder<T, S = {}, C = T, R extends string = DefaultRestParamsName> = (S extends T ? Buildable<T> : unknown) & {
+type _Builder<T, S = {}, C = T, R = never> = (S extends T ? Buildable<T> : unknown) & {
   [P in keyof C]-?: (
     P extends R
     ? (
       C[P] extends RestParams
-      ? (...o: NonNullable<C[P]>) => Builder<T, S & Record<P, C[P]>, Omit<C, P>, R>
+      ? (...o: NonNullable<C[P]>) => _Builder<T, S & Record<P, C[P]>, Omit<C, P>, R>
       : never
     )
-    : (o: C[P]) => Builder<T, S & Record<P, C[P]>, Omit<C, P>, R>
+    : (o: C[P]) => _Builder<T, S & Record<P, C[P]>, Omit<C, P>, R>
   )
 };
+
+/**
+ * @template T target 原型
+ * @template S selected 已选
+ * @template C candidate 候选
+ */
+export type Builder<T, S = {}, C = T> = _Builder<T, S, C, DefaultRestParamsName>;
 
 /**
  * 稳定的 Builder

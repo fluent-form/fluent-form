28933fa21c8b220c09a6c038d73ca875dbbb0b67
HyperLife1119 <hyperlife1119@qq.com>
2022-12-23 16:43:49 +0800

    refactor(directive): remove FluentTemplateOutletWithInjectorDirective

--- projects/ngx-fluent-form/src/directives/index.ts
+++ projects/ngx-fluent-form/src/directives/index.ts
@@ -3,4 +3,3 @@ export * from './form-name.directive';
 export * from './form.directive';
 export * from './outlet.directive';
 export * from './template-context-type.directive';
-export * from './template-outlet-with-injector.directive';

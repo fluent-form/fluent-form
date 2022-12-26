035bba6a8721414f6ab1b689334f00bd0c1d692c
HyperLife1119 <hyperlife1119@qq.com>
2022-12-27 00:15:06 +0800

    refactor(widget:date-range): typed template

--- projects/ngx-fluent-form/src/widgets/date-range/date-range.widget.ts
+++ projects/ngx-fluent-form/src/widgets/date-range/date-range.widget.ts
@@ -1,13 +1,15 @@
 import { NgClass, NgStyle } from '@angular/common';
 import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { FluentBinderDirective, FluentComposableDirective, FluentWithContextGuardDirective } from '../../directives';
 import { FluentCallPipe, FluentTypeofPipe } from '../../pipes';
-import { AbstractWidget, WidgetTemplateContext } from '../abstract.widget';
+import { FluentInvokePipe } from '../../pipes/invoke.pipe';
+import { DateRangePickerControlSchema } from '../../schemas';
+import { AbstractWidget, COL_HELPER, WidgetTemplateContext } from '../abstract.widget';
 
-type DateRangeWidgetTemplateContext = WidgetTemplateContext<any>;
+type DateRangeWidgetTemplateContext = WidgetTemplateContext<DateRangePickerControlSchema, FormControl<[Date, Date]>>;
 
 @Component({
   standalone: true,
@@ -21,11 +23,14 @@ type DateRangeWidgetTemplateContext = WidgetTemplateContext<any>;
     FluentWithContextGuardDirective,
     FluentComposableDirective,
     FluentTypeofPipe,
-    FluentCallPipe
+    FluentCallPipe,
+    FluentInvokePipe
   ],
   templateUrl: './date-range.widget.html',
   styles: [`nz-range-picker { width: 100% }`]
 })
 export class DateRangeWidget extends AbstractWidget<DateRangeWidgetTemplateContext> {
   @ViewChild(TemplateRef, { static: true }) templateRef!: TemplateRef<DateRangeWidgetTemplateContext>;
+
+  protected readonly helper = { col: COL_HELPER } as const;
 }

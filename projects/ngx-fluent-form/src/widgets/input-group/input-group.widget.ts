273bce708f9dd1488f1db44be8d086134a6e383e
HyperLife1119 <hyperlife1119@qq.com>
2022-12-27 14:49:49 +0800

    refactor(widget:input-group): typed template

--- projects/ngx-fluent-form/src/widgets/input-group/input-group.widget.ts
+++ projects/ngx-fluent-form/src/widgets/input-group/input-group.widget.ts
@@ -2,12 +2,14 @@ import { NgClass, NgFor, NgStyle, NgTemplateOutlet } from '@angular/common';
 import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { NzInputModule } from 'ng-zorro-antd/input';
-import { FluentControlOutletComponent } from '../../components';
 import { FluentBinderDirective, FluentWithContextGuardDirective, FluentWithInjectorDirective } from '../../directives';
 import { FluentControlPipe, FluentWidgetTemplateRefPipe } from '../../pipes';
+import { FluentInvokePipe } from '../../pipes/invoke.pipe';
+import { InputGroupComponentSchema } from '../../schemas';
+import { isString } from '../../utils';
 import { AbstractWidget, WidgetTemplateContext } from '../abstract.widget';
 
-type InputGroupWidgetTemplateContext = WidgetTemplateContext<any, FormGroup>;
+type InputGroupWidgetTemplateContext = WidgetTemplateContext<InputGroupComponentSchema, FormGroup>;
 
 @Component({
   standalone: true,
@@ -23,10 +25,17 @@ type InputGroupWidgetTemplateContext = WidgetTemplateContext<any, FormGroup>;
     FluentWithInjectorDirective,
     FluentControlPipe,
     FluentWidgetTemplateRefPipe,
-    FluentControlOutletComponent,
+    FluentInvokePipe,
   ],
   templateUrl: './input-group.widget.html',
 })
 export class InputGroupWidget extends AbstractWidget<InputGroupWidgetTemplateContext> {
   @ViewChild(TemplateRef, { static: true }) templateRef!: TemplateRef<InputGroupWidgetTemplateContext>;
+
+  protected readonly helper = {
+    addon: (addon: InputGroupComponentSchema['before'] | InputGroupComponentSchema['after']) =>
+      isString(addon) || addon instanceof TemplateRef ? addon : undefined,
+    addonIcon: (addon: InputGroupComponentSchema['before'] | InputGroupComponentSchema['after']) =>
+      isString(addon) || addon instanceof TemplateRef ? undefined : addon?.icon,
+  } as const;
 }

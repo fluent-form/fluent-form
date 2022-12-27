d6499c0dd6f1d383b0d4452fe103fa7345dabc8c
HyperLife1119 <hyperlife1119@qq.com>
2022-12-27 15:32:14 +0800

    refactor(widget:tree-select): typed template

--- projects/ngx-fluent-form/src/widgets/tree-select/tree-select.widget.ts
+++ projects/ngx-fluent-form/src/widgets/tree-select/tree-select.widget.ts
@@ -1,13 +1,17 @@
 import { NgClass, NgStyle } from '@angular/common';
 import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { SafeAny } from '@ngify/types';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzTreeSelectModule } from 'ng-zorro-antd/tree-select';
 import { FluentBinderDirective, FluentComposableDirective, FluentWithContextGuardDirective } from '../../directives';
 import { FluentCallPipe, FluentTypeofPipe } from '../../pipes';
-import { AbstractWidget, WidgetTemplateContext } from '../abstract.widget';
+import { FluentInvokePipe } from '../../pipes/invoke.pipe';
+import { TreeSelectControlSchema } from '../../schemas';
+import { isBoolean, isUndefined } from '../../utils';
+import { AbstractWidget, COL_HELPER, WidgetTemplateContext } from '../abstract.widget';
 
-type TreeSelectWidgetTemplateContext = WidgetTemplateContext<any>;
+type TreeSelectWidgetTemplateContext = WidgetTemplateContext<TreeSelectControlSchema, FormControl<SafeAny[]>>;
 
 @Component({
   standalone: true,
@@ -21,11 +25,24 @@ type TreeSelectWidgetTemplateContext = WidgetTemplateContext<any>;
     FluentWithContextGuardDirective,
     FluentComposableDirective,
     FluentTypeofPipe,
-    FluentCallPipe
+    FluentCallPipe,
+    FluentInvokePipe
   ],
   templateUrl: './tree-select.widget.html',
   styles: [`nz-tree-select { width: 100% }`]
 })
 export class TreeSelectWidget extends AbstractWidget<TreeSelectWidgetTemplateContext> {
   @ViewChild(TemplateRef, { static: true }) templateRef!: TemplateRef<TreeSelectWidgetTemplateContext>;
+
+  protected readonly helper = {
+    col: COL_HELPER,
+    checkable: (checkable: TreeSelectControlSchema['checkable']) =>
+      isBoolean(checkable) ? checkable : !isUndefined(checkable?.strict),
+    checkStrictly: (checkable: TreeSelectControlSchema['checkable']) =>
+      isBoolean(checkable) || !checkable?.strict ? false : checkable.strict,
+    showExpand: (expandIcon: TreeSelectControlSchema['expandIcon']) =>
+      isBoolean(expandIcon) ? expandIcon : true,
+    expandedIcon: (expandIcon: TreeSelectControlSchema['expandIcon']) =>
+      isBoolean(expandIcon) ? undefined : expandIcon
+  } as const;
 }

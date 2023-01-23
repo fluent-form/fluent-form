dff1b2dd90478ea5d39b0d99b604819dda9a7f9d
HyperLife1119 <hyperlife1119@qq.com>
2023-01-23 15:36:43 +0800

    refactor(feature): optimize feature function

--- projects/ngx-fluent-form/src/features.ts
+++ projects/ngx-fluent-form/src/features.ts
@@ -1,163 +1,190 @@
-import { Type } from '@angular/core';
+import { Provider, Type } from '@angular/core';
 import { WidgetKind } from './enumerations';
+import { WIDGET_MAP } from './tokens';
 import { AbstractWidget, ButtonGroupWidget, ButtonWidget, CascaderWidget, CheckboxGroupWidget, CheckboxWidget, DateRangeWidget, DateWidget, InputGroupWidget, InputWidget, NumberWidget, RadioGroupWidget, RateWidget, SelectWidget, SilderWidget, TextareaWidget, TextWidget, TimeWidget, ToggleWidget, TreeSelectWidget } from './widgets';
 
+export const enum FluentFormFeatureKind {
+  Widget
+}
+
 export interface FluentFormFeature {
+  kind: FluentFormFeatureKind;
+  providers: Provider[];
+}
+
+export interface FluentFormWidgetFeature {
   kind: WidgetKind;
   widget: Type<AbstractWidget<unknown>>;
 }
 
-export function withAllWidgets(): FluentFormFeature[] {
-  return [
-    withInputWidget(),
-    withInputGroupWidget(),
-    withTextareaWidget(),
-    withNumberWidget(),
-    withDateWidget(),
-    withDateRangeWidget(),
-    withTimeWidget(),
-    withToggleWidget(),
-    withSelectWidget(),
-    withCascaderWidget(),
-    withTreeSelectWidget(),
-    withSliderWidget(),
-    withRadioGroupWidget(),
-    withCheckboxWidget(),
-    withCheckboxGroupWidget(),
-    withRateWidget(),
-    withTextWidget(),
-    withButtonWidget(),
-    withButtonGroupWidget()
-  ];
+export function withAllWidgets(): FluentFormFeature {
+  return withWidgets(
+    useInputWidget(),
+    useInputGroupWidget(),
+    useTextareaWidget(),
+    useNumberWidget(),
+    useDateWidget(),
+    useDateRangeWidget(),
+    useTimeWidget(),
+    useToggleWidget(),
+    useSelectWidget(),
+    useCascaderWidget(),
+    useTreeSelectWidget(),
+    useSliderWidget(),
+    useRadioGroupWidget(),
+    useCheckboxWidget(),
+    useCheckboxGroupWidget(),
+    useRateWidget(),
+    useTextWidget(),
+    useButtonWidget(),
+    useButtonGroupWidget()
+  );
 }
 
-export function withInputWidget(): FluentFormFeature {
+export function withWidgets(...widgetFeatures: FluentFormWidgetFeature[]): FluentFormFeature {
+  return {
+    kind: FluentFormFeatureKind.Widget,
+    providers: [
+      {
+        provide: WIDGET_MAP,
+        useValue: new Map(
+          widgetFeatures.map(feature => [
+            feature.kind,
+            feature.widget
+          ])
+        )
+      }
+    ]
+  };
+}
+
+export function useInputWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Input,
     widget: InputWidget
   };
 }
 
-export function withInputGroupWidget(): FluentFormFeature {
+export function useInputGroupWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.InputGroup,
     widget: InputGroupWidget
   };
 }
 
-export function withTextareaWidget(): FluentFormFeature {
+export function useTextareaWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Textarea,
     widget: TextareaWidget
   };
 }
 
-export function withNumberWidget(): FluentFormFeature {
+export function useNumberWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Number,
     widget: NumberWidget
   };
 }
 
-export function withDateWidget(): FluentFormFeature {
+export function useDateWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Date,
     widget: DateWidget
   };
 }
 
-export function withDateRangeWidget(): FluentFormFeature {
+export function useDateRangeWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.DateRange,
     widget: DateRangeWidget
   };
 }
 
-export function withTimeWidget(): FluentFormFeature {
+export function useTimeWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Time,
     widget: TimeWidget
   };
 }
 
-export function withToggleWidget(): FluentFormFeature {
+export function useToggleWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Toggle,
     widget: ToggleWidget
   };
 }
 
-export function withSelectWidget(): FluentFormFeature {
+export function useSelectWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Select,
     widget: SelectWidget
   };
 }
 
-export function withCascaderWidget(): FluentFormFeature {
+export function useCascaderWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Cascader,
     widget: CascaderWidget
   };
 }
 
-export function withTreeSelectWidget(): FluentFormFeature {
+export function useTreeSelectWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.TreeSelect,
     widget: TreeSelectWidget
   };
 }
 
-export function withSliderWidget(): FluentFormFeature {
+export function useSliderWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Slider,
     widget: SilderWidget
   };
 }
 
-export function withRadioGroupWidget(): FluentFormFeature {
+export function useRadioGroupWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.RadioGroup,
     widget: RadioGroupWidget
   };
 }
 
-export function withCheckboxWidget(): FluentFormFeature {
+export function useCheckboxWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Checkbox,
     widget: CheckboxWidget
   };
 }
 
-export function withCheckboxGroupWidget(): FluentFormFeature {
+export function useCheckboxGroupWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.CheckboxGroup,
     widget: CheckboxGroupWidget
   };
 }
 
-export function withRateWidget(): FluentFormFeature {
+export function useRateWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Rate,
     widget: RateWidget
   };
 }
 
-export function withTextWidget(): FluentFormFeature {
+export function useTextWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Text,
     widget: TextWidget
   };
 }
 
-export function withButtonWidget(): FluentFormFeature {
+export function useButtonWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.Button,
     widget: ButtonWidget
   };
 }
 
-export function withButtonGroupWidget(): FluentFormFeature {
+export function useButtonGroupWidget(): FluentFormWidgetFeature {
   return {
     kind: WidgetKind.ButtonGroup,
     widget: ButtonGroupWidget
--- projects/ngx-fluent-form/src/provider.ts
+++ projects/ngx-fluent-form/src/provider.ts
@@ -1,18 +1,7 @@
 import { Provider } from '@angular/core';
 import { FluentFormFeature } from './features';
-import { WIDGET_MAP } from './tokens';
 
-export function provideFluentForm(...features: FluentFormFeature[] | FluentFormFeature[][]): Provider[] {
+export function provideFluentForm(...features: FluentFormFeature[]): Provider[] {
   // TODO: 升级到 v15 后使用 https://angular.cn/api/core/makeEnvironmentProviders 进行包装
-  return [
-    {
-      provide: WIDGET_MAP,
-      useValue: new Map(
-        features.flat().map(feature => [
-          feature.kind,
-          feature.widget
-        ])
-      )
-    }
-  ];
+  return features.map(feature => feature.providers);
 }

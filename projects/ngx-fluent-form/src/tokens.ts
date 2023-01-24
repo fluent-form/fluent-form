1d955998a179d6ed51415a70c7983a69717055eb
HyperLife1119 <hyperlife1119@qq.com>
2023-01-24 16:29:10 +0800

    feat: add config injection token

--- projects/ngx-fluent-form/src/tokens.ts
+++ projects/ngx-fluent-form/src/tokens.ts
@@ -1,5 +1,14 @@
 import { InjectionToken, Type } from '@angular/core';
+import { NzFormLayoutType } from 'ng-zorro-antd/form';
+import { NzRowDirective } from 'ng-zorro-antd/grid';
 import { WidgetKind } from './enumerations';
 import { AbstractWidget } from './widgets';
 
+export interface Config {
+  layout: NzFormLayoutType;
+  colon: boolean;
+  gutter: NzRowDirective['nzGutter'];
+}
+
 export const WIDGET_MAP = new InjectionToken<Map<WidgetKind, Type<AbstractWidget<unknown>>>>('WidgetMap');
+export const CONFIG = new InjectionToken<Config>('Config');

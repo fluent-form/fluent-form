1fb3ce29eec6f8294bfe6f7bfa00fd9e7b93aca2
HyperLife1119 <hyperlife1119@qq.com>
2023-01-26 22:16:30 +0800

    test(builder): adjust code

--- projects/ngx-fluent-form/src/builders/control-container.builder.spec.ts
+++ projects/ngx-fluent-form/src/builders/control-container.builder.spec.ts
@@ -1,24 +1,22 @@
-import { SafeAny } from '@ngify/types';
-import { FormGroupSchema } from '../schemas';
 import { standardSchema } from '../utils';
 import { array, form, group } from './control-container.builder';
 
 describe('control-container.builder', () => {
   it('form', () => {
     const schema = form(it => it.updateOn('blur').schemas());
-    const value = { kind: 'group', updateOn: 'blur', schemas: [] } as FormGroupSchema;
-    expect(schema).toEqual(value);
+
+    expect(schema).toEqual({ kind: 'group', updateOn: 'blur', schemas: [] });
   });
 
   it('group', () => {
     const schema = standardSchema(group('group').schemas());
-    const value = { kind: 'group', name: 'group', schemas: [] } as SafeAny;
-    expect(schema).toEqual(value);
+
+    expect(schema).toEqual({ kind: 'group', name: 'group', schemas: [] });
   });
 
   it('array', () => {
     const schema = standardSchema(array('array').schemas());
-    const value = { kind: 'array', name: 'array', schemas: [] } as SafeAny;
-    expect(schema).toEqual(value);
+
+    expect(schema).toEqual({ kind: 'array', name: 'array', schemas: [] });
   });
 });

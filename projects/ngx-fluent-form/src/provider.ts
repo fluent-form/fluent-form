854ae8160fc00258a4df42e747ff0760253b30d2
HyperLife1119 <hyperlife1119@qq.com>
2023-02-02 17:37:29 +0800

    chore: remove a todo

--- projects/ngx-fluent-form/src/provider.ts
+++ projects/ngx-fluent-form/src/provider.ts
@@ -2,6 +2,5 @@ import { Provider } from '@angular/core';
 import { FluentFormFeature } from './features';
 
 export function provideFluentForm(...features: FluentFormFeature[]): Provider[] {
-  // TODO: 升级到 v15 后使用 https://angular.cn/api/core/makeEnvironmentProviders 进行包装
   return features.map(feature => feature.providers);
 }

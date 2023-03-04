63e1e03ec560656797658605a8a32702192be0e1
hyperlife1119 <hyperlife1119@qq.com>
2023-03-05 01:59:30 +0800

    feat(utils): change standardSchema return type to StandardSchema

--- projects/ngx-fluent-form/src/utils/schema.utils.ts
+++ projects/ngx-fluent-form/src/utils/schema.utils.ts
@@ -2,17 +2,16 @@ import { ValidatorFn, Validators } from '@angular/forms';
 import { COMPONENT_CONTAINER_SCHEMA_KINDS, COMPONENT_SCHEMA_KINDS, COMPONENT_WRAPPER_SCHEMA_KINDS, CONTROL_CONTAINER_SCHEMA_KINDS, CONTROL_WRAPPER_SCHEMA_KINDS, TEXT_CONTROL_SCHEMA_KINDS } from '../constants';
 import { InputControlSchema, TextareaControlSchema } from '../schemas';
 import { AnyComponentContainerSchema, AnyComponentSchema, AnyComponentWrapperSchema, AnyContainerSchema, AnyControlContainerSchema, AnyControlOrControlContainerSchema, AnyControlSchema, AnyControlWrapperSchema, AnySchema, AnyWrapperSchema, DoubleKeyControlSchema } from '../schemas/index.schema';
-import { AnySchemaName, SchemaName } from '../schemas/types';
-import { isBuilder, StableBuilder } from './builder.utils';
+import { AnySchemaName, SchemaName, StandardSchema } from '../schemas/types';
+import { StableBuilder, isBuilder } from './builder.utils';
 import { isNumber } from './is.utils';
 
 /**
  * 是否为控件容器图示
  * @param schema
  */
-export const isControlContainerSchema = (schema: AnySchema): schema is AnyControlContainerSchema => (
-  CONTROL_CONTAINER_SCHEMA_KINDS.includes(schema.kind)
-);
+export const isControlContainerSchema = (schema: AnySchema): schema is AnyControlContainerSchema =>
+  CONTROL_CONTAINER_SCHEMA_KINDS.includes(schema.kind);
 
 /**
  * 是否为控件包装器图示
@@ -32,33 +31,29 @@ export const isComponentContainerSchema = (schema: AnySchema): schema is AnyComp
  * 是否为组件包装器图示
  * @param schema
  */
-export const isComponentWrapperSchema = (schema: AnySchema): schema is AnyComponentWrapperSchema => (
-  COMPONENT_WRAPPER_SCHEMA_KINDS.includes(schema.kind)
-);
+export const isComponentWrapperSchema = (schema: AnySchema): schema is AnyComponentWrapperSchema =>
+  COMPONENT_WRAPPER_SCHEMA_KINDS.includes(schema.kind);
 
 /**
  * 是否为文本图示
  * @param schema
  */
-export const isTextControlSchema = (schema: AnySchema): schema is InputControlSchema | TextareaControlSchema => (
-  TEXT_CONTROL_SCHEMA_KINDS.includes(schema.kind)
-);
+export const isTextControlSchema = (schema: AnySchema): schema is InputControlSchema | TextareaControlSchema =>
+  TEXT_CONTROL_SCHEMA_KINDS.includes(schema.kind);
 
 /**
  * 是否为组件图示
  * @param schema
  */
-export const isComponentSchema = (schema: AnySchema): schema is AnyComponentSchema => (
-  COMPONENT_SCHEMA_KINDS.includes(schema.kind)
-);
+export const isComponentSchema = (schema: AnySchema): schema is AnyComponentSchema =>
+  COMPONENT_SCHEMA_KINDS.includes(schema.kind);
 
 /**
  * 是否为双字段图示
  * @param schema
  */
-export const isDoubleKeyControlSchema = (schema: AnySchema): schema is DoubleKeyControlSchema => (
-  Array.isArray(schema.name)
-);
+export const isDoubleKeyControlSchema = (schema: AnySchema): schema is DoubleKeyControlSchema =>
+  Array.isArray(schema.name);
 
 /**
  * 标准化容器图示
@@ -82,8 +77,8 @@ const standardContainerSchema = <T extends AnyContainerSchema | AnyWrapperSchema
  * 标准化图示
  * @param schemaOrSchemaBuilder
  */
-export function standardSchema<T extends AnySchema>(schemaOrSchemaBuilder: T | StableBuilder<T>): T {
-  let schema = (isBuilder(schemaOrSchemaBuilder) ? schemaOrSchemaBuilder.build() : { ...schemaOrSchemaBuilder }) as AnySchema;
+export function standardSchema<T extends AnySchema>(schemaOrSchemaBuilder: T | StableBuilder<T>): StandardSchema<T> {
+  const schema = (isBuilder(schemaOrSchemaBuilder) ? schemaOrSchemaBuilder.build() : { ...schemaOrSchemaBuilder });
 
   if (
     isControlContainerSchema(schema) ||
@@ -127,16 +122,15 @@ export function standardSchema<T extends AnySchema>(schemaOrSchemaBuilder: T | S
     }
   }
 
-  return schema as T;
+  return schema as StandardSchema<T>;
 }
 
 /**
  * 标准化所有图示
  * @param schemas
  */
-export const standardSchemas = (schemas: (AnySchema | StableBuilder<AnySchema>)[]) => (
-  schemas.map(schema => standardSchema(schema))
-);
+export const standardSchemas = <T extends (AnySchema | StableBuilder<AnySchema>)[]>(schemas: T) =>
+  schemas.map(schema => standardSchema(schema));
 
 export function controlSchemaUtils<S extends AnyControlSchema>(schema: S) {
   return new ControlSchemaUtils(schema);

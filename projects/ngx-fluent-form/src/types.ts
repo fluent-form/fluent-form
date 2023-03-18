44026da54311eb0abfdac4bb15d83d60f475deac
hyperlife1119 <hyperlife1119@qq.com>
2023-03-18 16:36:52 +0800

    fix: fix type

--- projects/ngx-fluent-form/src/types.ts
+++ projects/ngx-fluent-form/src/types.ts
@@ -8,7 +8,7 @@ import { PickProperty, SafeAny } from '@ngify/types';
  * ```
  */
 export type HTMLElementEventListenerMap = {
-  [K in keyof HTMLElementEventMap]?: (event: HTMLElementEventMap[K]) => void | Promise<void>
+  [K in keyof HTMLElementEventMap]?: (event: HTMLElementEventMap[K]) => SafeAny
 };
 
 /**
@@ -37,7 +37,7 @@ type ComponentOutputName<C> = Exclude<
  * @template S 各种控件的图示
  */
 export type ComponentOutputListenerMap<C> = {
-  [K in ComponentOutputName<C>]?: (event: C[K] extends EventEmitter<infer E> ? E : never) => void | Promise<void>
+  [K in ComponentOutputName<C>]?: (event: C[K] extends EventEmitter<infer E> ? E : never) => SafeAny
 }
 
 /**

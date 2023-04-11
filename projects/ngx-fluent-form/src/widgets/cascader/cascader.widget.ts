15946d7f798782ff0570dd51a7fba7720908639f
hyperlife1119 <hyperlife1119@qq.com>
2023-04-11 22:56:16 +0800

    refactor(widget): migrate to FluentInjectDirective

--- projects/ngx-fluent-form/src/widgets/cascader/cascader.widget.ts
+++ projects/ngx-fluent-form/src/widgets/cascader/cascader.widget.ts
@@ -1,10 +1,11 @@
-import { NgClass, NgStyle } from '@angular/common';
+import { NgClass, NgIf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { SafeAny } from '@ngify/types';
 import { NzCascaderModule } from 'ng-zorro-antd/cascader';
+import { NzFormNoStatusService } from 'ng-zorro-antd/core/form';
 import { NzGridModule } from 'ng-zorro-antd/grid';
-import { FluentBindingDirective, FluentComposableDirective, FluentContextGuardDirective } from '../../directives';
+import { FluentBindingDirective, FluentContextGuardDirective, FluentInjectDirective } from '../../directives';
 import { FluentCallPipe, FluentColumnPipe } from '../../pipes';
 import { CascaderControlSchema } from '../../schemas';
 import { AbstractWidget, WidgetTemplateContext } from '../abstract.widget';
@@ -14,18 +15,21 @@ type CascaderWidgetTemplateContext = WidgetTemplateContext<CascaderControlSchema
 @Component({
   standalone: true,
   imports: [
+    NgIf,
     NgClass,
     NgStyle,
     ReactiveFormsModule,
     NzGridModule,
     NzCascaderModule,
+    FluentInjectDirective,
     FluentBindingDirective,
     FluentContextGuardDirective,
-    FluentComposableDirective,
     FluentCallPipe,
     FluentColumnPipe
   ],
   templateUrl: './cascader.widget.html',
   styles: [`nz-cascader { width: 100% }`]
 })
-export class CascaderWidget extends AbstractWidget<CascaderWidgetTemplateContext> { }
+export class CascaderWidget extends AbstractWidget<CascaderWidgetTemplateContext> {
+  protected readonly NzFormNoStatusService = NzFormNoStatusService;
+}

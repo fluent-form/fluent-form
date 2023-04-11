0b72153f38787890f40f5a9310f0a5197e0bffcc
hyperlife1119 <hyperlife1119@qq.com>
2023-04-11 22:58:18 +0800

    refactor(widget): migrate to FluentInjectDirective

--- projects/ngx-fluent-form/src/widgets/time/time.widget.ts
+++ projects/ngx-fluent-form/src/widgets/time/time.widget.ts
@@ -1,9 +1,10 @@
-import { NgClass, NgStyle } from '@angular/common';
+import { NgClass, NgIf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NzFormNoStatusService } from 'ng-zorro-antd/core/form';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
-import { FluentBindingDirective, FluentComposableDirective, FluentContextGuardDirective } from '../../directives';
+import { FluentBindingDirective, FluentContextGuardDirective, FluentInjectDirective } from '../../directives';
 import { FluentCallPipe, FluentColumnPipe } from '../../pipes';
 import { TimePickerControlSchema } from '../../schemas';
 import { AbstractWidget, WidgetTemplateContext } from '../abstract.widget';
@@ -13,18 +14,21 @@ type TimeWidgetTemplateContext = WidgetTemplateContext<TimePickerControlSchema, 
 @Component({
   standalone: true,
   imports: [
+    NgIf,
     NgClass,
     NgStyle,
     ReactiveFormsModule,
     NzGridModule,
     NzTimePickerModule,
+    FluentInjectDirective,
     FluentBindingDirective,
     FluentContextGuardDirective,
-    FluentComposableDirective,
     FluentCallPipe,
     FluentColumnPipe
   ],
   templateUrl: './time.widget.html',
   styles: [`nz-time-picker { width: 100% }`]
 })
-export class TimeWidget extends AbstractWidget<TimeWidgetTemplateContext> { }
+export class TimeWidget extends AbstractWidget<TimeWidgetTemplateContext> {
+  protected readonly NzFormNoStatusService = NzFormNoStatusService;
+}

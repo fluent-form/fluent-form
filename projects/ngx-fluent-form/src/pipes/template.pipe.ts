f081bce7bb0b14ec6cd2b6426bb68f4336bacfd2
hyperlife1119 <hyperlife1119@qq.com>
2023-04-27 16:57:39 +0800

    refactor: improve FluentTemplatePipe

--- projects/ngx-fluent-form/src/pipes/template.pipe.ts
+++ projects/ngx-fluent-form/src/pipes/template.pipe.ts
@@ -1,4 +1,7 @@
 import { inject, Pipe, PipeTransform, TemplateRef } from '@angular/core';
+import { CONTROL_CONTAINER_SCHEMA_KINDS } from '../constants';
+import { AbstractSchema } from '../schemas';
+import { AnySchemaName } from '../schemas/types';
 import { TemplateRegistry } from '../services';
 
 @Pipe({
@@ -8,8 +11,9 @@ import { TemplateRegistry } from '../services';
 export class FluentTemplatePipe implements PipeTransform {
   private readonly registry = inject(TemplateRegistry);
 
-  transform(value: string): TemplateRef<unknown> {
-    return this.registry.get(value);
+  transform(value: AbstractSchema<AnySchemaName>): TemplateRef<unknown> {
+    const kind = CONTROL_CONTAINER_SCHEMA_KINDS.includes(value.kind) ? 'nested-form' : value.kind;
+    return this.registry.get(kind);
   }
 
 }

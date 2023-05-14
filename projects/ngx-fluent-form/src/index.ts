1ac601a05217a9f1f2fcb77f275a21489a90c063
hyperlife1119 <hyperlife1119@qq.com>
2023-05-14 22:12:24 +0800

    chore: rename fluent-form.module to module

--- projects/ngx-fluent-form/src/index.ts
+++ projects/ngx-fluent-form/src/index.ts
@@ -6,7 +6,7 @@ export * from './builders';
 export * from './components';
 export * from './directives';
 export * from './features';
-export * from './fluent-form.module';
+export * from './module';
 export * from './provider';
 export * from './schemas';
 export * from './utils';

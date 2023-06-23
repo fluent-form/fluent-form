c1ac88207e083da087408e75c9fdbb998e554814
hyperlife1119 <hyperlife1119@qq.com>
2023-06-24 00:17:23 +0800

    test(widget): fix test

--- projects/ngx-fluent-form/src/widgets/form-array/form-array.widget.spec.ts
+++ projects/ngx-fluent-form/src/widgets/form-array/form-array.widget.spec.ts
@@ -7,6 +7,7 @@ import { FormArrayWidget } from './form-array.widget';
 
 describe('NestedFormWidget', () => {
   let component: FormArrayWidget;
+  let helper: FormArrayWidget['helper'];
   let fixture: ComponentFixture<FormArrayWidget>;
 
   beforeEach(() => {
@@ -19,6 +20,7 @@ describe('NestedFormWidget', () => {
     });
     fixture = TestBed.createComponent(FormArrayWidget);
     component = fixture.componentInstance;
+    helper = component['helper'];
     fixture.detectChanges();
   });
 
@@ -28,7 +30,7 @@ describe('NestedFormWidget', () => {
 
   it('add method', () => {
     const formArray = new FormArray<SafeAny>([]);
-    component.add(formArray, {
+    component['add'](formArray, {
       kind: 'array',
       schemas: [
         { kind: 'input' }
@@ -36,5 +38,21 @@ describe('NestedFormWidget', () => {
     });
 
     expect(formArray.length).toBe(1);
+  });
+
+  describe('helper function', () => {
+    it('length.min', () => {
+      expect(helper.length.min(undefined)).toBe(0);
+      expect(helper.length.min(1)).toBe(1);
+      expect(helper.length.min({ max: 1 })).toBe(0);
+      expect(helper.length.min({ min: 1 })).toBe(1);
+    });
+
+    it('length.max', () => {
+      expect(helper.length.max(undefined)).toBe(Infinity);
+      expect(helper.length.max(1)).toBe(1);
+      expect(helper.length.max({ min: 1 })).toBe(Infinity);
+      expect(helper.length.max({ max: 1 })).toBe(1);
+    });
   });
 });

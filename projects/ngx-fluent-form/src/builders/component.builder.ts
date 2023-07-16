337268f8660b53d79395c143b1ce91e1e564a445
hyperlife1119 <hyperlife1119@qq.com>
2023-07-16 18:39:25 +0800

    fix(builder): fix function sign of template

--- projects/ngx-fluent-form/src/builders/component.builder.ts
+++ projects/ngx-fluent-form/src/builders/component.builder.ts
@@ -2,7 +2,7 @@ import { ButtonComponentSchema, SchemaKey, TemplateSchema, TextComponentSchema }
 import { builder, UnstableBuilder } from '../utils';
 import { KindOrKey } from './helper';
 
-export function template<Key extends SchemaKey>(key?: Key): UnstableBuilder<TemplateSchema<Key>, KindOrKey> {
+export function template<Key extends SchemaKey>(key: Key): UnstableBuilder<TemplateSchema<Key>, KindOrKey> {
   return builder<TemplateSchema<Key>>().kind('template').key(key);
 }
 

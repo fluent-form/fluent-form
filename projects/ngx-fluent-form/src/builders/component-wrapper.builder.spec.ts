362fa811d845919d23c54cd67c65b61a9b83cad4
hyperlife1119 <hyperlife1119@qq.com>
2023-08-17 09:23:01 +0800

    refactor(builder): remove the simplified signature of buttonGroup

--- projects/ngx-fluent-form/src/builders/component-wrapper.builder.spec.ts
+++ projects/ngx-fluent-form/src/builders/component-wrapper.builder.spec.ts
@@ -5,7 +5,7 @@ import { button } from './component.builder';
 describe('component-wrapper.builder', () => {
   it('buttonGroup', () => {
     const schema1 = standardSchema(buttonGroup().schemas());
-    const schema2 = standardSchema(buttonGroup(button()));
+    const schema2 = standardSchema(buttonGroup().schemas(button()));
     expect(schema1).toEqual({ kind: 'button-group', schemas: [] });
     expect(schema2).toEqual({ kind: 'button-group', schemas: [{ kind: 'button' }] });
   });
--- projects/ngx-fluent-form/src/builders/component-wrapper.builder.ts
+++ projects/ngx-fluent-form/src/builders/component-wrapper.builder.ts
@@ -1,19 +1,13 @@
 import { AnyComponentWrapperSchema, ButtonGroupComponentSchema, SchemaKey } from '../schemas';
 import { Builder, builder, UnstableBuilder } from '../utils';
-import { isEmptyArray, isSchemaKeyTuple, KindOrKey, KindOrSchemas, RestSchema, REST_SCHEMA } from './helper';
+import { KindOrKey, RestSchema, REST_SCHEMA } from './helper';
 
 function componentWrapperBuilder<T extends AnyComponentWrapperSchema>(): Builder<T, RestSchema> {
   return builder<T, RestSchema>(REST_SCHEMA);
 }
 
-export function buttonGroup<Key extends SchemaKey>(key?: Key): UnstableComponentWrapperBuilder<ButtonGroupComponentSchema<Key>, KindOrKey>;
-export function buttonGroup(...schemas: ButtonGroupComponentSchema['schemas']): UnstableComponentWrapperBuilder<ButtonGroupComponentSchema, KindOrSchemas>;
-export function buttonGroup(...keyOrSchemas: [] | [SchemaKey] | ButtonGroupComponentSchema['schemas']) {
-  if (isEmptyArray(keyOrSchemas) || isSchemaKeyTuple(keyOrSchemas)) {
-    return componentWrapperBuilder<ButtonGroupComponentSchema>().kind('button-group').key(keyOrSchemas[0]);
-  }
-
-  return componentWrapperBuilder<ButtonGroupComponentSchema>().kind('button-group').schemas(...keyOrSchemas);
+export function buttonGroup<Key extends SchemaKey>(key?: Key): UnstableComponentWrapperBuilder<ButtonGroupComponentSchema<Key>, KindOrKey> {
+  return componentWrapperBuilder<ButtonGroupComponentSchema<Key>>().kind('button-group').key(key);
 }
 
 export type UnstableComponentWrapperBuilder<T extends AnyComponentWrapperSchema, S extends keyof T> = UnstableBuilder<T, S, RestSchema>;

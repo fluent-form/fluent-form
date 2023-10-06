c331a7c36bef145609e20f203b48972dc63263ce
hyperlife1119 <hyperlife1119@qq.com>
2023-10-06 16:04:51 +0800

    feat(pipe): improve FluentTemplatePipe

--- projects/ngx-fluent-form/src/pipes/template.pipe.ts
+++ projects/ngx-fluent-form/src/pipes/template.pipe.ts
@@ -10,7 +10,10 @@ import { isString } from '../utils';
 export class FluentTemplatePipe implements PipeTransform {
   private readonly container = inject(TEMPLATE_DIRECTIVE_CONTAINER, { optional: true });
 
-  transform(value?: string | TemplateRef<SafeAny>): string | TemplateRef<SafeAny> | undefined {
+  transform<T>(value: T): T | TemplateRef<SafeAny>
+  transform<T, U>(value: T, defaultValue: U): NonNullable<T> | U | TemplateRef<SafeAny>
+  transform<T, U>(value: T, defaultValue?: U): T | U | TemplateRef<SafeAny> {
+    // 如果是以 # 开头的字符串，则尝试查询外部注册的模板并返回
     if (isString(value) && value.startsWith('#')) {
       const name = value.slice(1); // remove the '#'
       const dir = this.container?.templateDirectives?.find(o => o.name === name);
@@ -20,7 +23,7 @@ export class FluentTemplatePipe implements PipeTransform {
       }
     }
 
-    return value;
+    return value ?? defaultValue!;
   }
 
 }

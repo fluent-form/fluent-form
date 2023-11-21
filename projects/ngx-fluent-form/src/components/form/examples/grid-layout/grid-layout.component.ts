26ef25f19580f4cf4c6dce02d4e4d3f89cfbe96e
hyperlife1119 <hyperlife1119@qq.com>
2023-11-21 17:14:57 +0800

    docs: update docs

--- projects/ngx-fluent-form/src/components/form/examples/grid-layout/grid-layout.component.ts
+++ projects/ngx-fluent-form/src/components/form/examples/grid-layout/grid-layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { alert, FluentFormComponent, form } from 'ngx-fluent-form';
 
 @Component({
+  selector: 'fluent-form-grid-layout-example',
   standalone: true,
   imports: [FluentFormComponent],
   template: `
     <fluent-form
-      justify="space-between"
+      [justify]="'space-between'"
       [align]="'middle'"
       [(model)]="model"
       [schema]="schema">

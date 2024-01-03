e5bae70a140e25652ce5cbfaa322f5511794c8c9
hyperlife1119 <hyperlife1119@qq.com>
2024-01-03 14:40:28 +0800

    refactor: simplify logic of isStaticExpression

--- projects/ngx-fluent-form/src/services/evaluator.service.ts
+++ projects/ngx-fluent-form/src/services/evaluator.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { AnyObject, SafeAny } from '@ngify/types';
 
 const RETURN_STR = 'return ';
-const INTERPOLATION: [string, string] = ['{{', '}}'];
 
 export abstract class CodeEvaluator {
   abstract evaluate(code: string, context: AnyObject): SafeAny;
 }
 
 export function isStaticExpression(str: string) {
-  return str.startsWith(INTERPOLATION[0]) && str.endsWith(INTERPOLATION[1]);
+  return /^{{.+}}$/.test(str);
 }
 
 /**
@@ -21,7 +20,7 @@ export class DynamicCodeEvaluator implements CodeEvaluator {
   evaluate(code: string, context: AnyObject) {
     code = RETURN_STR + code.replace(/^{{|}}$/g, '');
 
-    const fn = new Function('ctx', `with(ctx){${code}}`);
+    const fn = new Function('o', `with(o){${code}}`);
     const proxy = new Proxy(Object.freeze(context), {
       // 拦截所有属性，防止到 Proxy 对象以外的作用域链查找。
       has() {

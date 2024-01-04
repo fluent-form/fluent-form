2cdf23aeb4e33f7c53e96cf2ee1072d9c25e4b6c
hyperlife1119 <hyperlife1119@qq.com>
2024-01-04 14:33:23 +0800

    fix: fix some bugs

--- projects/ngx-fluent-form/src/index.ts
+++ projects/ngx-fluent-form/src/index.ts
@@ -9,6 +9,7 @@ export * from './directives';
 export * from './features';
 export * from './interfaces';
 export * from './module';
+export * from './patcher';
 export * from './provider';
 export * from './schemas';
 export * from './utils';

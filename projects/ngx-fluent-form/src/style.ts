7a1d8d2751567e2df84740d60126d78818048a9d
hyperlife1119 <hyperlife1119@qq.com>
2024-02-25 21:42:51 +0800

    fix: fix with style function

--- projects/ngx-fluent-form/src/style.ts
+++ projects/ngx-fluent-form/src/style.ts
@@ -1,6 +1,8 @@
-import { EnvironmentInjector, Type, createComponent, inject } from '@angular/core';
+import { DestroyRef, EnvironmentInjector, Type, createComponent, inject } from '@angular/core';
 
 export function withStyle(component: Type<unknown>): void {
   const environmentInjector = inject(EnvironmentInjector);
-  createComponent(component, { environmentInjector });
+  const destroyRef = inject(DestroyRef);
+  const componentRef = createComponent(component, { environmentInjector });
+  destroyRef.onDestroy(() => componentRef.destroy());
 }

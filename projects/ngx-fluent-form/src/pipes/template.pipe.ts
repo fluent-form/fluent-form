0e691943bfe56d5f672c2863b0f959076cc889f5
hyperlife1119 <hyperlife1119@qq.com>
2024-03-20 14:16:20 +0800

    refactor(pipe): optimize single character check

--- projects/ngx-fluent-form/src/pipes/template.pipe.ts
+++ projects/ngx-fluent-form/src/pipes/template.pipe.ts
@@ -17,7 +17,7 @@ export class FluentTemplatePipe implements PipeTransform {
   transform<T, U>(value: T, defaultValue: U): NonNullable<T> | U | TemplateRef<SafeAny>
   transform<T, U>(value: T, defaultValue?: U): T | U | TemplateRef<SafeAny> {
     // 如果是以 # 开头的字符串，则尝试查询外部注册的模板并返回
-    if (isString(value) && value.startsWith('#')) {
+    if (isString(value) && value[0] === '#') {
       const name = value.slice(1); // remove the '#'
       const dir = this.templateDirectives?.find(o => o.name === name);
 

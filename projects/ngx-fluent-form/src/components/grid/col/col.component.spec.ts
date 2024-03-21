257f7f2640c33a8ac31158c02fabb941ec1d1361
hyperlife1119 <hyperlife1119@qq.com>
2024-03-21 10:15:00 +0800

    test(grid): fix test

--- projects/ngx-fluent-form/src/components/grid/col/col.component.spec.ts
+++ projects/ngx-fluent-form/src/components/grid/col/col.component.spec.ts
@@ -5,7 +5,7 @@ import { FluentColDirective } from './col.component';
 @Component({
   standalone: true,
   imports: [FluentColDirective],
-  template: `<fluent-col [span]="span" [flex]="flex" [offset]="offset" />`,
+  template: `<fluent-column [span]="span" [flex]="flex" [offset]="offset" />`,
 })
 class TestComponent {
   @ViewChild(FluentColDirective, { read: ElementRef, static: true })
@@ -30,40 +30,40 @@ describe('FluentColComponent', () => {
   });
 
   it('should have default class', () => {
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col')).toBeTrue();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column')).toBeTrue();
   });
 
   it('should be able parse the span', () => {
     component.span = 1;
     fixture.detectChanges();
     fixture.detectChanges();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-1')).toBeTrue();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-1')).toBeTrue();
 
     component.span = { xxl: 1 };
     fixture.detectChanges();
     fixture.detectChanges();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-1')).toBeFalse();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-1')).toBeFalse();
   });
 
   it('should be able parse the offset', () => {
     component.offset = 1;
     fixture.detectChanges();
     fixture.detectChanges();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-offset-1')).toBeTrue();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-offset-1')).toBeTrue();
 
     component.offset = null;
     fixture.detectChanges();
     fixture.detectChanges();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-offset-1')).toBeFalse();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-offset-1')).toBeFalse();
 
     component.offset = { xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 };
     fixture.detectChanges();
     fixture.detectChanges();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-offset-1')).toBeTrue();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-offset-sm-2')).toBeTrue();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-offset-md-3')).toBeTrue();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-offset-lg-4')).toBeTrue();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-offset-xl-5')).toBeTrue();
-    expect(component.colElementRef.nativeElement.classList.contains('fluent-col-offset-xxl-6')).toBeTrue();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-offset-1')).toBeTrue();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-offset-sm-2')).toBeTrue();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-offset-md-3')).toBeTrue();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-offset-lg-4')).toBeTrue();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-offset-xl-5')).toBeTrue();
+    expect(component.colElementRef.nativeElement.classList.contains('fluent-column-offset-xxl-6')).toBeTrue();
   });
 });

0c9c5e18626fd967d3ed1b64e1df316807de2dff
hyperlife1119 <hyperlife1119@qq.com>
2024-08-07 15:11:25 +0800

    feat(core): update withSchemaPatchers to support nested arrays of patchers

--- packages/core/src/lib/features/schema-patcher.feature.ts
+++ packages/core/src/lib/features/schema-patcher.feature.ts
@@ -3,9 +3,9 @@ import { SchemaPatcher, provideSchemaPatcher } from '../patcher';
 import { makeFluentFeature } from './helper';
 import { FluentFormFeature, FluentFormFeatureKind } from './interface';
 
-export function withSchemaPatchers(patchers: SchemaPatcher[]): FluentFormFeature<FluentFormFeatureKind.SchemaPatcher> {
+export function withSchemaPatchers(patchers: SchemaPatcher[] | SchemaPatcher[][]): FluentFormFeature<FluentFormFeatureKind.SchemaPatcher> {
   return makeFluentFeature(
     FluentFormFeatureKind.SchemaPatcher,
-    patchers.map<Provider>(patcher => provideSchemaPatcher(patcher))
+    patchers.flat().map<Provider>(patcher => provideSchemaPatcher(patcher))
   );
 }

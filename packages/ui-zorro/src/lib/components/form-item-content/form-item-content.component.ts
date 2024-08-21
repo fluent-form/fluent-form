6bb2e11287e724caf0b9da2e0838fbf3e9aaaf5f
hyperlife1119 <hyperlife1119@qq.com>
2024-08-21 18:59:46 +0800

    refactor(ui-zorro): support for empty label

--- packages/ui-zorro/src/lib/components/form-item-content/form-item-content.component.ts
+++ packages/ui-zorro/src/lib/components/form-item-content/form-item-content.component.ts
@@ -1,7 +1,7 @@
 import { NgTemplateOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { AbstractFormItemContentComponent, FluentControlPipe, FluentReactivePipe, FluentSchemaPipe, FluentSchemaTypePipe, FluentTemplatePipe, FluentVarDirective, FluentWidgetTemplatePipe, FluentWithInjectorDirective, InvokePipe, SchemaType } from '@fluent-form/core';
-import { NzFormModule } from 'ng-zorro-antd/form';
+import { AbstractFormItemContentComponent, FluentControlPipe, FluentInjectDirective, FluentReactivePipe, FluentSchemaPipe, FluentSchemaTypePipe, FluentTemplatePipe, FluentVarDirective, FluentWidgetTemplatePipe, FluentWithInjectorDirective, InvokePipe, SchemaType } from '@fluent-form/core';
+import { NzFormDirective, NzFormModule } from 'ng-zorro-antd/form';
 import { labelHelper, tooltipHelper } from '../../helper';
 
 /**
@@ -14,6 +14,7 @@ import { labelHelper, tooltipHelper } from '../../helper';
     NzFormModule,
     FluentWithInjectorDirective,
     FluentVarDirective,
+    FluentInjectDirective,
     FluentReactivePipe,
     FluentSchemaPipe,
     FluentControlPipe,
@@ -26,6 +27,7 @@ import { labelHelper, tooltipHelper } from '../../helper';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormItemContentComponent extends AbstractFormItemContentComponent {
+  protected readonly NzFormDirective = NzFormDirective;
   protected readonly SchemaType = SchemaType;
 
   protected readonly helper = {

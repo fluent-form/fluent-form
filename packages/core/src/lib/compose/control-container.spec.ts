d1081e33a4449069b11f1c477958c301b44e0d4f
HyperLife1119 <hyperlife1119@qq.com>
2025-04-29 16:22:45 +0800

    refactor(core): remove form(builder) function signature

--- packages/core/src/lib/compose/control-container.spec.ts
+++ packages/core/src/lib/compose/control-container.spec.ts
@@ -1,16 +1,10 @@
 import { signal } from '@angular/core';
-import { group, textField } from '../testing';
+import { textField } from '../testing';
 import { applyRoot } from './builder';
 import { fluentForm, form } from './control-container';
 
 describe('control-container', () => {
   describe('form', () => {
-    it('factory', () => {
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      const scheam = form(group().updateOn('blur').schemas(() => { }));
-      expect(scheam).toEqual({ kind: 'group', key: 'root', updateOn: 'blur', schemas: [] });
-    });
-
     it('schemas', () => {
       const { schemas } = form([{ kind: 'text-field', key: 'input' }]);
       expect(schemas).toEqual([{ kind: 'text-field', key: 'input' }]);
--- packages/core/src/lib/compose/control-container.ts
+++ packages/core/src/lib/compose/control-container.ts
@@ -1,34 +1,26 @@
 import { computed, Signal } from '@angular/core';
 import { SafeAny } from '@ngify/types';
-import { AbstractControlContainerSchema, AbstractFormGroupSchema, AbstractSchema } from '../schemas';
+import { AbstractFormGroupSchema, AbstractSchema } from '../schemas';
 import { Indexable } from '../types';
-import { isArray, isFunction } from '../utils';
-import { composeBuilder, StableBuilder } from './builder';
+import { isArray } from '../utils';
+import { composeBuilder } from './builder';
 
 export function form(composeFn: FormComposeFn): AbstractFormGroupSchema;
 export function form(schemas: Indexable<AbstractSchema>[]): AbstractFormGroupSchema;
-export function form(builder: StableBuilder<AbstractControlContainerSchema>): AbstractFormGroupSchema;
-export function form(fnOrSchemasOrBuilder: Indexable<AbstractSchema>[] | FormComposeFn | StableBuilder<AbstractControlContainerSchema>): AbstractFormGroupSchema {
-  if (isArray(fnOrSchemasOrBuilder)) {
+export function form(fnOrSchemas: Indexable<AbstractSchema>[] | FormComposeFn): AbstractFormGroupSchema {
+  if (isArray(fnOrSchemas)) {
     return {
       kind: 'group',
       key: 'root',
-      schemas: fnOrSchemasOrBuilder,
+      schemas: fnOrSchemas,
     };
   }
 
-  if (isFunction(fnOrSchemasOrBuilder)) {
-    return composeBuilder<AbstractFormGroupSchema>()
-      .kind('group')
-      .key('root')
-      .schemas(fnOrSchemasOrBuilder)
-      .build();
-  }
-
-  const scheam = fnOrSchemasOrBuilder.build() as AbstractFormGroupSchema;
-  scheam.key = 'root';
-
-  return scheam;
+  return composeBuilder<AbstractFormGroupSchema>()
+    .kind('group')
+    .key('root')
+    .schemas(fnOrSchemas)
+    .build();
 }
 
 type FormComposeFn = () => SafeAny;

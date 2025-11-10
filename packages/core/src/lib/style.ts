debdd83f47288a43e9bf3d50a6bc0ab10cdcdb34
HyperLife1119 <hyperlife1119@qq.com>
2025-11-10 09:23:29 +0800

    refactor(core/grid): refactor grid components to use cdk style loader

--- packages/core/src/lib/style.ts
+++
@@ -1,8 +0,0 @@
-import { DestroyRef, EnvironmentInjector, Type, createComponent, inject } from '@angular/core';
-
-export function withStyle(component: Type<unknown>): void {
-  const environmentInjector = inject(EnvironmentInjector);
-  const destroyRef = inject(DestroyRef);
-  const componentRef = createComponent(component, { environmentInjector });
-  destroyRef.onDestroy(() => componentRef.destroy());
-}
